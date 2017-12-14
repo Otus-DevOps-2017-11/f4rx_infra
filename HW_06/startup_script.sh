42ab2c244169be9c44c98ee301f09f4aa19ff8ee
Aleksey Stepanenko <stepanenko@selectel.ru>
2017-12-15 01:34:48 +0300

    HW06

--- HW_06/startup_script.sh
+++ HW_06/startup_script.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+cat > /tmp/install_puma.sh << 'EOF'
+#!/bin/bash
+
+cd ~/
 git clone https://github.com/Otus-DevOps-2017-11/f4rx_infra.git && \
 cd f4rx_infra && \
 git checkout Infra-2 && \
 ./HW_06/install_ruby.sh && \
 ./HW_06/install_mongodb.sh && \
 ./HW_06/deploy.sh
+
+EOF
+
+sudo -i -u astepanenko bash /tmp/install_puma.sh
