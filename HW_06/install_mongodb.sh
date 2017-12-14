be8d72226928714231fe435a3af8d27594532ba6
Aleksey Stepanenko <stepanenko@selectel.ru>
2017-12-15 01:02:27 +0300

    HW06

--- HW_06/install_mongodb.sh
+++ HW_06/install_mongodb.sh
@@ -10,12 +10,15 @@ err_report() {
 
 trap err_report ERR
 
+echo "Install mongo"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
 sudo apt install -y mongodb-org
 
+echo "Start mongo"
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
+echo "Check mongo"
 pgrep mongod
